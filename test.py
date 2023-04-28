19fdb6fb27056b675da502255d62e424b975a7aa
shyam chow <shyammannuru98@gmail.com>
2023-04-28 17:18:56 +0530

    modified test.py file

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-hello guys 
-this class is about github
+ These are some git commands
+ * git log --> It will give commit msg,ID,email,author
+
+ * git show (commit id) --> this will entire details of committed file
+
+ * git show --pretty="" --name-only (id) --> this will give file name only
+
+ * git commit -a -m "commit msg" --> this directly move the updated file from working area to local repository
+ 
+ * git commit --amend -m "commit msg" --> to change the last commit msg 
+
