efa630eb23855e4fd6b1cc3bc67fe048babde283
shyam chow <shyammannuru98@gmail.com>
2023-04-28 15:32:07 +0530

    committing the modified file

--- DButils.java
+++ DButils.java
@@ -1,1 +1,16 @@
 "Hello guys this file contains DB connection logic"
+Adding codes like
+1.cd desktop
+2.mkdir master 
+3.git init
+4.vi DButils.java
+5.git status
+6.git add (file name) or git add *.java
+7.git config --global user.name
+8.git config --global user.email
+9.git config --global --list
+10.git config --global user.name "shyamchow" --> to change the user name
+11.git commit -m "commit msg"
+12.git commit --amend -m "you can update the last commit msg"
+13.git log --> to see the commit msg
+
