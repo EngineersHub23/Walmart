7b01860e6e0277eff116d0b5f37b35ee4b747be1
shyam chow <shyammannuru98@gmail.com>
2023-04-28 17:49:50 +0530

    added the updated file also new file to staging area

--- DButils.java
+++ DButils.java
@@ -12,5 +12,11 @@ Adding codes like
 10.git config --global user.name "shyamchow" --> to change the user name
 11.git commit -m "commit msg"
 12.git commit --amend -m "you can update the last commit msg"
-13.git log --> to see the commit msg
-
+13.git log --> to see the list commit id,msg,author
+14.git -a -m "commit msg" --> you can directly move only the modified file from working area to local repo
+15.git push wm(remote repo alias name) master(local repo master branch) ---> it is used to push the files in local repo to remote repo
+16.git show (commit ID) --> shows the commit details (you can get the commit id by giving git log)
+17.git show --pretty="" --name-only (commit id) --> it will give only the file name of that particular commit id
+18.touch (file name)
+19.git reset test.c --> it will move the file from staging area to working area
+20.
